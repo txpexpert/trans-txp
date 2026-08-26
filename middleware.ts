5d16afb6b8bb86d4386272abaa6fd3c5a1ad6f62
txpexpert <txp.expert@gmail.com>
2026-08-26 10:34:37 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -52,6 +52,6 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api/|backoffice/|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api/|backoffice$|backoffice/|_next/static|_next/image|favicon.ico).*)',
   ],
 }
